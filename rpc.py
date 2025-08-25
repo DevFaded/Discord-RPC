451f6347815a4e59479295409adbe4da5e5ca158
Faded <granveer308@gmail.com>
2025-08-25 20:20:45 +1200

    Create rpc.py

---
+++ rpc.py
@@ -0,0 +1,21 @@
+from pypresence import Presence
+import time
+
+RPC = Presence("clienid")
+RPC.connect()
+RPC.update(
+    details="nah bro",
+    state="in nigga",
+    large_image="largeimagekei", 
+    large_text="nobro",
+    small_image="smallimagekei",
+    small_text="popadise"
+)
+
+print("rpc running")
+try:
+    while True:
+        time.sleep(15)  
+except KeyboardInterrupt:
+    RPC.close()
+    print("rpc bomobmo")
